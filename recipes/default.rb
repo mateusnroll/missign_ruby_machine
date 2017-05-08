7e73265fb59df328b3f0e1e2e2864bb473e21806
Mateus Pinheiro <mateusp@br.ibm.com>
2017-05-08 18:18:34 -0300

    Install all basic packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+# Install nginx
+yum_repository 'nginx' do
+	description 'nginx'
+	baseurl 'http://nginx.org/packages/mainline/centos/7/$basearch/'
+	gpgcheck false
+	enabled true
+end
+
+package 'nginx'
+
+
+# Install ruby 2.4.0
+include_recipe "ruby_rbenv::system"
+include_recipe "ruby_build::default"
+
+rbenv_ruby "2.4.0"
+rbenv_global "2.4.0"
+
+
+# Install Puma
+rbenv_gem "puma"
