eccfd729daa27d3cd048b7984597e965dfc113a9
Mateus Pinheiro <mateusp@br.ibm.com>
2017-05-10 15:43:54 -0300

    Fix the metadata

--- metadata.rb
+++ metadata.rb
@@ -8,10 +8,10 @@ version '0.1.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/missing_ruby_machine/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/mateusnroll/missing_ruby_machine/issues' if respond_to?(:issues_url)
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/missing_ruby_machine' if respond_to?(:source_url)
+source_url 'https://github.com/mateusnroll/missing_ruby_machine' if respond_to?(:source_url)
 
 depends 'ruby_rbenv'
